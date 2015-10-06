381da3262d6ee5e7dfe8d0014bb3ce7dd092425f
Mark Marsella <mmarsella114@g.rwu.edu>
2015-10-06 11:14:38 -0700

    crud made

--- app.js
+++ app.js
@@ -26,11 +26,86 @@ app.use(session({
 
 app.use(loginMiddleware);  // calling the loginhelper on EVERY REQUEST!
 
+ var songIds = ["995535015", "966411602", "823593456", "956689796", "943946671",
+                 "982388023", "907242704", "201281527", "656801339", "910038357",
+                 "250038575", "878000348",  "794095205",  "1645339",  "400835962",
+                 "325618", "169003415",  "51958108",
+                 "76532142", "192688540", "684811768", "344799464", "217633921",
+                 "192811017", "640047583", "517438248" ];
 
 
-app.get("/", function(req, res) {
-  res.render("toberemoved");
+function getRandomSong(){
+  var randNum = Math.floor((Math.random() * songIds.length));
+  return songIds[randNum];
+}
+
+/*********** LOGIN AND SIGN UP ***************
+*********************************************/
+
+app.get("/signup", routeMiddleware.preventLoginSignup, function (req,res){
+  res.render("users/signup");
 });
+
+app.post("/signup", function (req,res){
+  var newUser = req.body.user;
+  db.User.create(newUser, function (err,user){
+    console.log(user);
+    if(user){
+      req.login(user);
+      res.redirect("/"); //redirect user to homepage
+    }else{
+      console.log(err);
+      res.redirect("/signup");
+    }
+  });
+});
+
+app.get("/login", function (req,res){
+  res.render("users/login");
+});
+
+app.post("/login", function (req,res){
+  db.User.authenticate(req.body.user,
+    function (err,user){
+      console.log("THE USER: " + user);
+      if(!err && user !== null){
+        req.login(user);
+        res.redirect("/users/" + user._id);
+      }else{
+        res.redirect("/users");
+      }
+    });
+});
+
+app.get("/logout", function (req,res){
+  req.logout();
+  res.redirect("/");
+});
+
+/********** GAME ROUTES ***************/
+
+//redirects to the game
+app.get("/", function(req, res) {
+  res.redirect("/randomsong");
+});
+
+//GO TO THE GAME
+app.get("/randomsong", function (req,res){
+  request.get("https://itunes.apple.com/us/lookup?id=" + getRandomSong(), function (err,response,body){
+    var songData = JSON.parse(body).results[0];
+    var data = {song:songData, score: scoreData};
+    var scoreData = {id: req.query.id, name: req.query.name, score: req.query.score};
+  res.render("randomsong", data);
+  });
+});
+
+
+
+
+
+
+
+
 
 
 
