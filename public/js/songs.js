de941c42c3614de84256709034889b7999408d88
Mark Marsella <mmarsella114@g.rwu.edu>
2015-10-07 09:45:30 -0700

    song plays on page load

--- public/js/songs.js
+++ public/js/songs.js
@@ -1,4 +1,5 @@
 $(function() {
+  $("audio")[0].play();  // PLAY A SONG WHEN THE PAGE LOADS
 
    //The user input
   var input;
@@ -9,11 +10,13 @@ $(function() {
   console.log("track: " + trackName);
 
   // Link the button.  On click... play audio
-$("#button").on("click", function(){
+$("#button").on("click", function(e){
+  // e.preventDefault();  // FORM WILL NOT SUBMIT!!!
   $("#answer").attr("style","display: none");  
   console.log("BUTTON CLICKED");
-  $("audio")[0].play();
   console.log(artist);
+  $("#scoreName").val(trackName);  // assign trackname to hidden input
+  $("#scoreScore").val(winCount);
   });
 
   //USERS GUESS
@@ -55,47 +58,11 @@ function checkWin(input)
 }
 
 //HIDDEN FORM
-$("#hiddenForm").on("submit", function (e){
-  e.preventDefault();
-  $("#scoreName").val(trackName);  // assign trackname to hidden input
-  $("#scoreScore").val(winCount);
-});
+// $("#hiddenForm").on("submit", function (e){
 
-/******* CHART ***********/
-
-var scoreArray = $('.score').map(function() {return $(this).val(); });
-
-var nameArray = $('.name').map(function() {return $(this).val(); });
-// Pass user data into a bar chart for scores and names
-// labels = names
+// });
 
 
-
-var data = {
-    labels: nameArray,
-    datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(0,255,0,0.3)",
-            strokeColor: "rgba(220,220,220,0.8)",
-            highlightFill: "rgba(220,220,220,0.75)",
-            highlightStroke: "rgba(220,220,220,1)",
-            data: scoreArray
-        },
-        // {
-        //     label: "My Second dataset",
-        //     fillColor: "rgba(151,187,205,0.5)",
-        //     strokeColor: "rgba(151,187,205,0.8)",
-        //     highlightFill: "rgba(151,187,205,0.75)",
-        //     highlightStroke: "rgba(151,187,205,1)",
-        //     data: [28, 48, 40, 19, 86, 27, 90]
-        // }
-    ]
-};
-
-// Get the context of the canvas element we want to select
-var ctx = document.getElementById("myChart").getContext("2d");
-var myNewChart = new Chart(ctx).Bar(data);
 
 
 
